0cd20f9e43785d5ef3ff3d4e0b22507d4475e399
prasadk <prasadkatepk@gmail.com>
2024-09-12 23:25:51 +0530

    feat: styled-components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import styled from "styled-components";
 import "./App.css";
+import { ButtonProps } from "./common/interface";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <p>Styled components setup</p>
+      <ButtonContainer className="button-container">
+        <Button primary>Submit</Button>
+        <Button>Submit</Button>
+      </ButtonContainer>
+    </>
+  );
 }
 
 export default App;
+
+const Button = styled.button<ButtonProps>`
+  padding: 10px 20px;
+  cursor: pointer;
+  &:hover {
+    background: ${({ primary }) => (primary ? "blue" : "green")};
+    color: white;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
